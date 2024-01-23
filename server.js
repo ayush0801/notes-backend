f4a05b4425b55a1d6e61053f90d20cff40cff6d5
Ayush <jha.4@iitj.ac.in>
2024-01-24 01:02:15 +0530

    implemented logic for update a note

--- server.js
+++ server.js
@@ -16,18 +16,21 @@ const notes = []
 //temp
 app.get('/', (req, res) => {
     res.status(200).json('This is the message that i want to display at localhost:3000/')
-})
+})  
 
 // Get all notes
-app.get('/notes', (req, res) => {
-    // Implement logic to retrieve all notes
-    res.status(200).json('This is the homepage where all the notes will be displayed')
-});
+app.get('/notes', async(req, res) => {
+    try {
+        const notes = await notesModel.find();  
+        res.status(200).json({notes});
+    } catch (err) {
+        res.json({error : err.message})
+    }
+})
 
 //Get a particular note
 app.get('/notes/:id', async(req, res) => {   
     // Implement logic to retrieve a particular note
-
     const noteID = req.params.id;
     if (!ObjectId.isValid(noteID)) {
         return res.status(400).json({ error: 'Invalid note ID format' });
@@ -37,63 +40,78 @@ app.get('/notes/:id', async(req, res) => {
         res.status(200).json({note: data});
     })
     .catch((err) => {
-        console.log(err);
+        // console.log(err);
         res.status(400).json({error: err.message})
     })
 
 });
 
-// app.get('/notes/:id', async (req, res) => {
-//     const noteId = req.params.id;
-
-//     notesModel.findOne({ _id: noteId })
-//         .then((note) => {
-//             if (!note) {
-//                 return res.status(404).json({ message: 'Note not found' });
-//             }
-
-//             res.status(200).json({ note: note });
-//         })
-//         .catch((error) => {
-//             res.status(500).json({ error: error.message });
-//         });
-// });
-
 // Create a new note
 app.post('/notes', async(req, res) => {
-    // Implement logic to create a new note
     const {title, body} = req.body
-
     if(!title || !body){
       return res.status(400).json({error:'Please provide both title and body'});
     }
-
     try {
         const newNote = new notesModel({title, body});
-        // console.log(newNote)
-        // newNote.save()
-        // .then((data) => {
-        //     res.send(201).json({message: "Note created successfully with ID: " + data._id})
-        // })
-        // .catch((err) => {
-        //     // console.log(err)
-        //     res.status(500).json({error: err.message})
-        // })
-        await newNote.save();
-
+        await newNote.save();   
         res.status(201).json({message: "Note created successfully with ID: " + newNote._id})
     } catch (err) {
-        // console.log(err)
-        // console.log(err)
         res.status(500).json({error: err.message})
-        
     }   
 });
 
 // Update a note by ID
 app.put('/notes/:id', (req, res) => {
     // Implement logic to update a note by ID
-    
+    const id = req.params.id
+    const thingsToUpdate = {}
+    const {title, body} = req.body
+    // if(title && body){
+    //     if(title.length < 3){
+    //         return res.status(422).json({error: "Title should be atleast 3 characters"})
+    //     }
+    //     if(body.length < 10){
+    //         return res.status(422).json({error: "Body should be atleast 10 characters"})
+    //     }
+    //     thingsToUpdate.title = title
+    //     thingsToUpdate.body = body
+    // }
+    // else if(title){
+    //     if(title.length < 3){
+    //         return res.status(422).json({error: "Title should be atleast 3 characters"})
+    //     }
+    //     thingsToUpdate.title = title
+    // }
+    // else if(body){
+    //     if(body.length < 10){
+    //         return res.status(422).json({error: "Body should be atleast 10 characters"})
+    //     }
+    //     thingsToUpdate.body = body
+    // }
+    // else{
+    //     return res.status(422).json({error: "Please fill any one field"})
+    // }
+    if(title.length < 3){
+        return res.status(422).json({error: "Title should be atleast 3 characters"})
+    }
+    if(body.length < 10){
+        return res.status(422).json({error: "Body should be atleast 10 characters"})
+    }
+    thingsToUpdate.title = title
+    thingsToUpdate.body = body
+    notesModel.findOneAndUpdate({_id:id}, {$set: thingsToUpdate})
+    .then((data) => {
+        if(!data){
+            res.status(404).json({error: "No data found"})
+        }
+        else{
+            res.status(200).json({message: "Note Updated Successfully"})
+        }
+    })
+    .catch((err) => {
+        res.status(500).json({error: err.message})
+    })
 });
 
 // Delete a note by ID
